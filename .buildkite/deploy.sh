2521471cb9afb71002aa311c32a7221bfd40b3b4
Keith Pitt <me@keithpitt.com>
2016-02-16 17:01:27 +1100

    Only deploy the emoji folders

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "--- :s3: Deploying emojis to $S3_BUCKET_PATH"
 
-s3cmd put -P --recursive --verbose --force --no-preserve . "$S3_BUCKET_PATH"
+s3cmd put -P --recursive --verbose --force --no-preserve "img-apple-64" "$S3_BUCKET_PATH"
+s3cmd put -P --recursive --verbose --force --no-preserve "img-buildkite-64" "$S3_BUCKET_PATH"
 
 echo "All done! 💪"
