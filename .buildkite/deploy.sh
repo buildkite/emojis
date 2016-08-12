01ed802a6890e758ec609a99ac654d4366bb23d5
Tim Lucas <t@toolmantim.com>
2016-08-12 17:30:55 +1000

    Fix the aws s3 dir

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "--- :s3: Deploying emojis to $S3_BUCKET_PATH"
 
-aws s3 sync --region "us-east-1" --acl "public-read" "img-apple-64" "$S3_BUCKET_PATH"
-aws s3 sync --region "us-east-1" --acl "public-read" "img-buildkite-64" "$S3_BUCKET_PATH"
+aws s3 sync --region "us-east-1" --acl "public-read" "img-apple-64" "$S3_BUCKET_PATH/img-apple-64"
+aws s3 sync --region "us-east-1" --acl "public-read" "img-buildkite-64" "$S3_BUCKET_PATH/img-buildkite-64"
 
 echo "All done! 💪"
